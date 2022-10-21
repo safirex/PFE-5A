cb93a9a2cdbe7d507f3e236827e825e0706f34fc
chatinEu <eudes.chatin@gmail.com>
2022-10-21 08:17:03 +0200

    modif table init for usage

--- src/db/dbschemes.py
+++ src/db/dbschemes.py
@@ -1,5 +1,6 @@
 from sqlalchemy import BIGINT, VARCHAR, Date, Integer, PrimaryKeyConstraint, Table, Text
 import sys
+from db.dbConnection import get_engine
 sys.path.append('..')
 from files_format_enums import *
 from sqlalchemy import MetaData, Table, String, Column, Text, DateTime, Boolean
@@ -30,7 +31,7 @@ pk_dict[GTFSFilenames.shapes]           =  ["shape_id","shape_pt_sequence"]
 pk_dict[GTFSFilenames.stops]            =  ["stop_id"]       
 pk_dict[GTFSFilenames.stop_times]       =  ["stop_id","trip_id"]       
 pk_dict[GTFSFilenames.trips]            =  ["trip_id"]   
-pk_dict[GTFSFilenames.RT]               =  {'trip' : ["trip_id","timestamp"], 'stop': ["stop_id"]}   
+pk_dict[GTFSFilenames.RT]               =  {'trip' : ["trip_id","timestamp"], 'stop': ["trip_id","stop_id"]}   
 
 
 
@@ -41,20 +42,52 @@ def get_db_table_pkeys(tablename :GTFSFilenames):
 
 
 
-
+# trips_rt = Table('trips',metadata,
+#     Column('trip_id',VARCHAR(50),primary_key=True),
+#     Column('route_id',VARCHAR(50)),
+#     Column('direction_id',BIGINT),
+#     Column('timestamp',BIGINT,primary_key=True),
+#     Column('vehicle_id',VARCHAR(50)),
+#     Column('vehicle_label',VARCHAR(50)),
+#     Column('stop_id',VARCHAR(50),primary_key=True),
+#     Column('arrival_delay',BIGINT),
+#     Column('arrival_time',BIGINT),
+#     Column('arrival_delay',BIGINT),
+#     Column('departure_delay',BIGINT),
+#     Column('departure_time',BIGINT),
+# )
 metadata = MetaData()
-trips_rt = Table('trips',metadata,
-    Column('trip_id',VARCHAR(50),primary_key=True),
-    Column('route_id',VARCHAR(50)),
-    Column('direction_id',BIGINT),
-    Column('timestamp',BIGINT,primary_key=True),
-    Column('vehicle_id',VARCHAR(50)),
-    Column('vehicle_label',VARCHAR(50)),
-    Column('stop_id',VARCHAR(50),primary_key=True),
-    Column('arrival_delay',BIGINT),
-    Column('arrival_time',BIGINT),
-    Column('arrival_delay',BIGINT),
-    Column('departure_delay',BIGINT),
-    Column('departure_time',BIGINT),
-)
+table_dict = {}
 
+def get_data_tables():
+    
+    engine = get_engine()
+    metadata.reflect(engine)
+    trips_rt = Table('rt_trip_info',metadata,
+        Column('trip_id',VARCHAR(50),primary_key=True),
+        Column('route_id',VARCHAR(50)),
+        Column('direction_id',BIGINT),
+        Column('timestamp',BIGINT,primary_key=True),
+        Column('vehicle_id',VARCHAR(50)),
+        Column('vehicle_label',VARCHAR(50))
+    )
+
+    stop_rt = Table('rt_stop_info',metadata,
+        Column('trip_id',VARCHAR(50),primary_key=True),
+        Column('stop_id',VARCHAR(50),primary_key=True),
+        Column('arrival_delay',BIGINT),
+        Column('arrival_time',BIGINT),
+        Column('arrival_delay',BIGINT),
+        Column('departure_delay',BIGINT),
+        Column('departure_time',BIGINT),
+    )
+
+    table_dict['trip'] = trips_rt
+    table_dict['stop'] = stop_rt
+
+def get_table_schema(name:str):
+    return table_dict[name]
+
+def create_tables():
+    engine = get_engine()
+    metadata.create_all(engine)
