622a6a7205bcd5baf98da13f9fca8d16d268caa8
chatinEu <eudes.chatin@gmail.com>
2022-11-17 07:29:02 +0100

    modify trigger for timestamp

--- sql/psql_raw_data_trigger.sql
+++ sql/psql_raw_data_trigger.sql
@@ -4,11 +4,11 @@ CREATE or REPLACE FUNCTION insert_trigger() RETURNS trigger AS $$
     BEGIN
         IF NOT EXISTS (SELECT 1 FROM rt_trip_info WHERE trip_id = NEW.trip_id) THEN
         INSERT INTO rt_trip_info(trip_id,route_id,direction_id,timestamp,vehicle_id,vehicle_label)
-        VALUES (    new.trip_id,new.route_id,new.direction_id,new.timestamp,new.vehicle_id,new.vehicle_label);
+        VALUES (new.trip_id,new.route_id,new.direction_id,to_timestamp(new.timestamp),new.vehicle_id,new.vehicle_label);
         END if;
         IF NOT EXISTS (SELECT 1 FROM rt_stop_info WHERE trip_id = NEW.trip_id AND stop_id = NEW.stop_id ) THEN
         INSERT INTO rt_stop_info(trip_id,stop_id,arrival_delay,arrival_time,departure_delay,departure_time,scheduled_relationship)
-        VALUES ( new.trip_id,new.stop_id,new.arrival_delay,new.arrival_time,new.departure_delay,new.departure_time,NEW.scheduled_relationship);
+        VALUES ( new.trip_id,new.stop_id,new.arrival_delay,to_timestamp(new.arrival_time),new.departure_delay,to_timestamp(new.departure_time),NEW.scheduled_relationship);
         END if;
         RETURN NEW;
     END;
